fec6d1322cb2f662cb8bab808b7932fe87fccebb
Darwel Quintero <darwel34@gmail.com>
2017-05-22 07:40:55 -0430

    se agrego la funcionalidad para que el menu se cierre una vez hecho click sobre alguna opcion

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ $('[data-toggle-dia]').click( function (ev){
     const panel = $(this).data('toggleDia');
     $('#lineup-tabs').foundation('selectTab',panel);
 });
+
+const $offCanvas = $('#offCanvas');
+$offCanvas.find('li').click(function (ev){
+    $offCanvas.foundation('close');
+});
